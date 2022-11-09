0f1c99901e71a9f038cc77cd3c122b059b2f791a
Breno Aguiar <breno.boaventura321@gmail.com>
2022-11-09 20:52:43 -0300

    exercicio 03

---
+++ 03/index.js
@@ -0,0 +1,4 @@
+let valorProduto = 110, meuDinheiro = 50;
+let valorDesconto = ((valorProduto-meuDinheiro)*100)/valorProduto;
+
+console.log(`O valor do desconto necessario é de: ${valorDesconto.toFixed(1)}%`);
