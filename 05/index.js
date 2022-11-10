be8901662297934d86c8c17b3ab927d4a3214bb4
Breno Aguiar <breno.boaventura321@gmail.com>
2022-11-09 21:43:52 -0300

    exercicio 05

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,7 @@
+let x1= 1, x2= 1, y1 = 1, y2 = 4;
 
+let distancia = Math.sqrt(
+    ((x2-x1)**2) + ((y2-y1)**2)
+);
+
+console.log(`A distancia entre o Ponto (x1, y1) e o ponto (x2. y2) é de ${distancia} `);
