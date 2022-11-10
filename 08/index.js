c7dcedf8d1e924077fca9ca6c91af5489de4236a
breno.boaventura321 <breno.boaventura321@gmail.com>
2022-11-10 09:54:21 -0300

    Exercicio 08

---
+++ 08/index.js
@@ -0,0 +1,7 @@
+let raio = 1, pi = Math.PI;
+
+let comprimento = (2*pi)*raio;
+
+let area = pi*(raio**2);
+
+console.log(`O comprimento é de ${comprimento.toFixed(2)}, e a area é de ${area.toFixed(2)}`);
